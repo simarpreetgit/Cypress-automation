3ea5af0da0a8502585619be4330205eb9c49087a
Simarpreet <preet091989@gmail.com>
2022-10-12 19:15:29 +0200

    first commit tablebookingtest

---
+++ cypress/e2e/Tablebooking.js
@@ -0,0 +1,22 @@
+
+/// <reference types="Cypress" />
+
+describe("Test contact Us form via Wbdriver uni",()=>{
+
+    it("Should be able to submit a succesful submition of tablebooking ",()=>{
+
+        // cypress code
+  
+        cy.visit ('https://singh-restro.netlify.app/')
+        cy.get('.me-auto > .btn').click()
+        cy.get('.form-row > :nth-child(1) > .form-control').type('Simarpreet Singh')  
+        cy.get(':nth-child(2) > .form-control').type('09099')
+        cy.get('#inputmessage4').type('table booking for 2 peron for today')
+        cy.get('.form-row > :nth-child(3) > .btn').click()
+
+    })
+
+
+
+})
+
