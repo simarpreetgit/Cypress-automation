f9da26e195c1d541aee03ec112c20370260a3bb5
Simarpreet <preet091989@gmail.com>
2022-10-14 14:22:47 +0200

    firsttestcase

---
+++ cypress/e2e/Automation-test-store/Contact-us.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress-xpath" />
+
+describe('Test contact Us form via automation store', () => {
+  it('Should be able to submit a succesful submition of contact us', () => {
+    cy.visit('https://automationteststore.com')
+
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    cy.title().should('include', 'A place to practice your automation skills!')
+    cy.url().should('include', '')
+    cy.xpath("//a[contains(@href ,'contact')]").click()
+    cy.get('#ContactUsFrm_first_name').type('helloo')
+    cy.get('#ContactUsFrm_email').type('abc@gmail.com')
+    cy.get('#ContactUsFrm_enquiry').type(
+      'learning from automation from udemy by gianni',
+    )
+    cy.xpath("//button[contains(@title,'Submit')]").click()
+    cy.get('.mb40 > :nth-child(3)').should(
+      'have.text',
+      'Your enquiry has been successfully sent to the store owner!',
+    )
+  })
+})
