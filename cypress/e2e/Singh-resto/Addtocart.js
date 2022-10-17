a573f6956f2cb720c4aecc7dd00f31947d78aa01
Simarpreet <preet091989@gmail.com>
2022-10-17 16:41:15 +0200

    alert handling

--- cypress/e2e/Singh-resto/Addtocart.js
+++ cypress/e2e/Singh-resto/Addtocart.js
@@ -15,5 +15,30 @@ describe('Iterate over elements', () => {
   it.only('log info of all hair care products', () => {
     cy.visit('https://singh-restro.netlify.app/')
     cy.contains('.card-body', 'Cheese Burger').find('.btn').click()
+    cy.get('.swal-text').should(
+      'have.text',
+      'Your Cheese Burger added to cart!',
+    )
   })
+  // it.only('Validate js confirm alert box using stub', () => {
+  //   cy.visit('http://www.webdriveruniversity.com/')
+  //   cy.get('#popup-alerts')
+  //     .invoke('removeAttr', 'target')
+  //     .click({ force: true })
+  //   const stub = cy.stub()
+  //   cy.on('window:confirm', stub)
+
+  //   cy.get('#button4')
+  //     .click()
+  //     .then(() => {
+  //       expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+  //     })
+
+  //     .then(() => {
+  //       return true
+  //     })
+  //     .then(() => {
+  //       cy.get('#confirm-alert-text').contains('You pressed OK!')
+  //     })
+  // })
 })
---
+++ cypress/e2e/Webdriveruni/Js-alert.js
@@ -0,0 +1,60 @@
+/// <reference types = "Cypress" />
+
+describe('handle js alerts', () => {
+  it('Confirm js alert contains the correct text', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#popup-alerts')
+      .invoke('removeAttr', 'target')
+      .click({ force: true })
+    cy.get('#button1').click()
+    cy.on('window:alert', (str) => {
+      expect(str).to.eq('I am an alert box!')
+    })
+  })
+
+  it('Validate js confirm alert box works correctly when clicking ok', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#popup-alerts')
+      .invoke('removeAttr', 'target')
+      .click({ force: true })
+    cy.get('#button4').click()
+    cy.on('window:confirm', (str) => {
+      return true
+    })
+    cy.get('#confirm-alert-text').contains('You pressed OK!')
+  })
+
+  it('Validate js confirm alert box works correctly when clicking cancel', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#popup-alerts')
+      .invoke('removeAttr', 'target')
+      .click({ force: true })
+    cy.get('#button4').click()
+    cy.on('window:confirm', (str) => {
+      return false
+    })
+    cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+  })
+
+  it.only('Validate js confirm alert box using stub', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#popup-alerts')
+      .invoke('removeAttr', 'target')
+      .click({ force: true })
+    const stub = cy.stub()
+    cy.on('window:confirm', stub)
+
+    cy.get('#button4')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+      })
+
+      .then(() => {
+        return true
+      })
+      .then(() => {
+        cy.get('#confirm-alert-text').contains('You pressed OK!')
+      })
+  })
+})
