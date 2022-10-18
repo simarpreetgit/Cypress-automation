f7f739465ed7932a5ba0c54d6a0c72d372dec9f8
Simarpreet <preet091989@gmail.com>
2022-10-18 21:33:54 +0200

    radio button

---
+++ cypress/e2e/Webdriveruni/Radio-buttons.js
@@ -0,0 +1,39 @@
+/// <reference types = "Cypress" />
+
+describe('Verify checkboxes via webdriver uni', () => {
+  it('Check and validate checkbox', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons')
+      .invoke('removeAttr', 'target')
+      .click({ force: true })
+    // using value
+    //cy.get('[type="radio"]').check('blue')
+    // using first function
+    cy.get('#radio-buttons')
+      .find('[type="radio"]')
+      .first()
+      .check()
+      .should('be.checked')
+    // using index select radio
+    cy.get('#radio-buttons')
+      .find('[type="radio"]')
+      .eq(1)
+      .check()
+      .should('be.checked')
+  })
+
+  it('Validates the states of specific radio buttons', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons')
+      .invoke('removeAttr', 'target')
+      .click({ force: true })
+
+    cy.get('[value="lettuce"]').should('be.enabled')
+
+    cy.get('[value="lettuce"]').should('not.be.checked')
+    cy.get('[value="pumpkin"]').should('be.checked')
+    cy.get('[value="cabbage"]').should('be.disabled')
+
+    cy.get('[value="lettuce"]').check().should('be.checked')
+  })
+})
